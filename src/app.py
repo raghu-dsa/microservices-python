2eb699962f13775c7c72540bb5cd6e58b038aaf1
raghu-dsa <raghavreddy1122@gmail.com>
2023-04-02 11:00:09 -0400

    flask app

---
+++ src/app.py
@@ -0,0 +1,30 @@
+
+from flask import Flask,jsonify,render_template
+import socket
+
+app = Flask(__name__)
+
+#Function to fetch hostname and IP
+def fetchdetails():
+    hostname=socket.gethostname()
+    host_ip=socket.gethostbyname(hostname)
+    return str(hostname),str(host_ip) 
+
+@app.route("/")
+def hello_world():
+    # print("i am here")
+    return "<p>Hello, World!</p>"
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status= "UP"
+    )
+
+@app.route("/details")
+def details():
+    hostname,ip=fetchdetails()
+    return render_template('index.html',HOSTNAME=hostname,IP=ip)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
